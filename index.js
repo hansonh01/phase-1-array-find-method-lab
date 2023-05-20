646082fdc7579a9c859e865a62f869b7805c5f55
Hanson <hanson.hua903@gmail.com>
2023-05-20 17:04:16 -0500

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record){
+    let foundWins = record.find(record => record.result === "W");
+    if (foundWins){
+        return foundWins.year;
+    }
+    return undefined;
+}
